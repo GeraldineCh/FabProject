13f08c2f3b7e0862d3175f563213c4a36f45004d
RuthMery <ruth.cardenas.perez@gmail.com>
2017-05-22 14:35:36 -0500

    For Mobile - Imágenes responsive

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 $(document).foundation()
+// EVENTOS
+window.addEventListener("load", function (){
+  resizeImage();
+});
+window.addEventListener("resize", function (){
+  resizeImage();
+});
+// FUNCIONES
+// Obtiene el ancho de una imagen, y establece hight = width
+function resizeImage (){
+  var catalogue_images = document.querySelectorAll("#catalogue .img-container img");
+  var base_width = String(catalogue_images[0].offsetWidth + "px");
+  for (var i = 0; i < catalogue_images.length; i++) {
+    catalogue_images[i].style.height = base_width;
+  }
+  return;
+}
