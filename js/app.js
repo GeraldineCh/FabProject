d74b51b377154d6797354b36e4a56eece7685228
RuthMery <ruth.cardenas.perez@gmail.com>
2017-05-23 13:42:11 -0500

    Reparando conflictos

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).foundation()
 // EVENTOS
 window.addEventListener("load", function (){
@@ -17,50 +16,3 @@ function resizeImage (){
   }
   return;
 }
-=======
-//$(document).foundation()
-
-/*$(document).ready(function() {
-	var owl = $('.owl-carousel');
-	owl.owlCarousel({
-		margin: 10,
-		nav: true,
-		loop: true,
-		responsive: {
-			0: {
-				items: 1
-			},
-			600: {
-				items: 3
-			},
-			1000: {
-				items: 5
-			}
-		}
-	})
-})*/
-
-
-
-
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-
-    $(document).on('ready', function() {
-   $(".regular").slick({
-     infinite: true,
-     slidesToShow: 5,
-     slidesToScroll: 5,
-     draggable: false
-   });
- });
-
-  /*  $(document).on('ready', function() {
-     $('.your-class').slick({
-       dots: true,
-       infinite: true,
-       slidesToShow: 3,
-       slidesToScroll: 3
-     });*/
->>>>>>> carousel
---
+++ js/carousel.js
@@ -0,0 +1,8 @@
+$(document).on('ready', function() {
+   $(".regular").slick({
+     infinite: true,
+     slidesToShow: 5,
+     slidesToScroll: 5,
+     draggable: false
+   });
+ });
